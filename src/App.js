4a2d9feb6b797588928406a1e53f67cb3d085ad6
Sean Glancy <spglancy@gmail.com>
2019-04-16 17:27:42 -0700

    converted styles to viewport relative

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from "react"
 import Nav from "./components/nav"
+import Project from "./components/projects"
 import "./App.css"
 
 const App = () => {
   return (
-    <div className='App'>
-      <Nav />
-      <div className='about'>
-        <h2>
-          Hi, I'm <span>Sean Glancy</span>
-        </h2>
-        <p>
-          I am a <span>Front-end web developer</span> who currently lives in San
-          Francisco, California. I enjoy making websites and yata yata yata yata
-          yata yata
-        </p>
-        <div>
-          <button>View Resume</button>
-          <button>Contact Sean</button>
+    <div className='container'>
+      <div className='App'>
+        <Nav />
+        <div className='about'>
+          <h2>
+            Hi, I'm <span>Sean Glancy</span>
+          </h2>
+          <p>
+            I am a <span>Front-end web developer</span> who currently lives in
+            San Francisco, California. I enjoy making websites and yata yata
+            yata yata yata yata
+          </p>
+          <div>
+            <button>View Resume</button>
+            <button>Contact Sean</button>
+          </div>
+        </div>
+        <div className='lower'>
+          <p>Scroll Down to See my Projects</p>
+          <img src='/arrow.svg' />
         </div>
       </div>
-      <div className='lower'>
-        <p>Scroll Down to See my Projects</p>
-        <img src='/arrow.svg' />
+      <div id='projects'>
+        <Project />
+        <Project />
+        <Project />
       </div>
     </div>
   )
---
+++ src/components/projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Project = props => {
+  return (
+    <div className='project'>
+      <img src='#' />
+    </div>
+  )
+}
+
+export default Project
