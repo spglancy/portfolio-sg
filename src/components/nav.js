905ed56816e9e613765b7d5526848c5f4ce2fee1
Sean Glancy <spglancy@gmail.com>
2020-01-21 15:51:37 -0800

    added working mobile nav and reversi

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,37 +1,26 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import './nav.css'
 
-class Nav extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-  }
-  expand() {
-
-  }
-
-  render() {
-    return (
-      <nav>
-        <div>
-          <h2>Sean Glancy</h2>
-          <div onClick={() => this.expand()} className="hamburger">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+function Nav() {
+  const [open, setOpen] = useState(false)
+  return (
+    <nav>
+      <div>
+        <h2>Sean Glancy</h2>
+        <div onClick={() => setOpen(!open)} className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <ul id="menu">
-          <li><a href='#banner'>Home<div></div></a></li>
-          <li><a href='#projects'>Projects<div></div></a></li>
-          <li><a href='#contact'>Contact<div></div></a></li>
-          <li><a href='https://drive.google.com/file/d/1eZGpBocTfZT-w35n_7RPVHg_9Er1v-1D/view?usp=sharing' rel='noopener noreferrer' target='_blank'>Resume<div></div></a></li>
-        </ul>
-      </nav >
-    )
-  }
+      </div>
+      <ul id="menu" className={open ? "open" : ""}>
+        <li><a href='#banner'>Home<div></div></a></li>
+        <li><a href='#projects'>Projects<div></div></a></li>
+        <li><a href='#contact'>Contact<div></div></a></li>
+        <li><a href='https://drive.google.com/file/d/1eZGpBocTfZT-w35n_7RPVHg_9Er1v-1D/view?usp=sharing' rel='noopener noreferrer' target='_blank'>Resume<div></div></a></li>
+      </ul>
+    </nav >
+  )
 }
 
 export default Nav
