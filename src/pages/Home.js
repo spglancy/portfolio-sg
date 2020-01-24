b77dcdc724dd7208b2aa119acdd2319571da78bd
Sean Glancy <spglancy@gmail.com>
2020-01-24 15:47:36 -0800

    added info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="banner" id="banner">
       <h1>Hi, I'm Sean Glancy</h1>
-      <p>I am a Frontend Web Developer who is passionate about building webapps with new and exciting technologies.</p>
+      <p>I am a Frontend Web Developer, currently based in San Francisco, who is passionate about building webapps with new and exciting technologies.</p>
     </div>
   )
 }
