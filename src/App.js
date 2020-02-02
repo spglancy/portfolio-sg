099ba8a28c2cc23e9ca728dfc85dc67703153d5a
Sean Glancy <spglancy@gmail.com>
2020-02-01 23:22:34 -0800

    started adding a footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Projects from "./pages/projects"
 import Home from "./pages/Home"
 import Nav from './components/nav'
+import Footer from "./components/footer"
 import "./App.css"
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Nav />
       <Home />
       <Projects />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+function Footer() {
+  return (
+    <div className="footer">
+      <ul>
+        <li>temp</li>
+        <li>temp</li>
+        <li>temp</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
