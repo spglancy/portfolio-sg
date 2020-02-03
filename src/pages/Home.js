65362e3eda7516e6aa7e2e39d0787348353ad07a
Sean Glancy <spglancy@gmail.com>
2020-02-02 19:27:19 -0800

    basic banner design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import './home.css'
 const Home = () => {
   return (
     <div className="banner" id="banner">
-      <h1>Hi, I'm Sean Glancy</h1>
-      <p>I am a Frontend Web Developer, currently based in San Francisco, who is passionate about building webapps with new and exciting technologies. My technologies of choice include React.js, MongoDB, Express.js, and Node.js.</p>
+      <h1>Sean Glancy</h1>
+      <span>Fullstack Developer | San Francisco</span>
     </div>
   )
 }
